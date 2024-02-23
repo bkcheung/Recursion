9ef3cdeab3ec6bf9c2647e2c038bf79da1aba8c1
bkcheung <b.cheung@hotmail.com>
2024-02-23 17:48:43 -0500

    Add recursive fibonacci function

--- fib.js
+++ fib.js
@@ -1,10 +1,19 @@
 console.log(fibs(8));
+console.log(fibsRec(8));
 
 //Iterative approach
 function fibs(n){
     let arr = [0, 1];
     for(let i=2;i<n;i++){
         arr.push(arr[i-2]+arr[i-1]);
+    }
+    return arr;
+}
+//Recursive approach
+function fibsRec(n){
+    let arr = [0,1];
+    for(let i=2; i<n; i++){
+        arr[i] = arr[i-1] + arr[i-2];
     }
     return arr;
 }
